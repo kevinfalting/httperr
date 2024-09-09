24e324f78bc1e78ce2ebea4cfbcec8065418a52e
Kevin Falting <kevin.falting@agilebits.com>
2024-09-09 11:14:10 -0500

    remove error handling implementations
    After some conversations with colleagues, the error handling
    implementation provided here is too restrictive. The magic lies in the
    different types and wrapping functions. There are too many different
    ways a project may need to handle the errors returned from handlers.
    
    A project could:
    - have different encodings
    - different localizations
    
    just to name a few that came up.
    
    A project importing this (or copy pasting it to avoid a little
    dependency) will need to implement it's own way of handling errors.

--- httperr.go
+++ httperr.go
@@ -1,12 +1,7 @@
 package httperr
 
 import (
-	"errors"
-	"fmt"
-	"io"
 	"net/http"
-	"os"
-	"strings"
 )
 
 // Handler responds to an http request and can return an error.
@@ -67,84 +62,4 @@ func WrapCommonToStd(toStd ToStd, common ...Middleware) func(HandlerFunc, ...Mid
 func WrapToStd(h HandlerFunc, toStd ToStd, mw ...Middleware) http.Handler {
 	handler := Wrap(h, mw...)
 	return toStd(handler)
-}
-
-// ErrFunc defines the function signature required to handle error responses.
-// Modeled from the [http.Error] function.
-type ErrFunc func(w http.ResponseWriter, err string, code int)
-
-// HandleErr returns a [ToStd] by providing a way to handle all errors before
-// passing back to a [http.Handler] for compatability with the stdlib.
-func HandleErr(errWriter io.Writer, errFunc ErrFunc) ToStd {
-	if errWriter == nil {
-		errWriter = os.Stderr
-	}
-
-	if errFunc == nil {
-		errFunc = http.Error
-	}
-
-	return func(h Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			err := h.ServeHTTP(w, r)
-			if err == nil {
-				return
-			}
-
-			var e interface{ StatusMsg() (int, string) }
-			if errors.As(err, &e) {
-				status, msg := e.StatusMsg()
-				errFunc(w, msg, status)
-			} else {
-				errFunc(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-			}
-
-			fmt.Fprint(errWriter, err)
-		})
-	}
-}
-
-type handlerError struct {
-	err         error
-	status      int
-	responseMsg string
-}
-
-// StatusMsg will return the http status code and message to return to the
-// client.
-func (h *handlerError) StatusMsg() (int, string) {
-	return h.status, h.responseMsg
-}
-
-// Error satisfies the error interface
-func (h *handlerError) Error() string {
-	return fmt.Sprintf("status=%d msg=%q err=%q", h.status, h.responseMsg, h.err)
-}
-
-// Is reports whether any error in err's tree matches target.
-func (h *handlerError) Is(target error) bool {
-	return errors.Is(h.err, target)
-}
-
-// As finds the first error in err's tree that matches target, and if one is
-// found, sets target to that error value and returns true. Otherwise, it
-// returns false.
-func (h *handlerError) As(target any) bool {
-	return errors.As(h.err, target)
-}
-
-// Unwrap returns the underlying error
-func (h *handlerError) Unwrap() error {
-	return h.err
-}
-
-// NewError will return an error that can be used by the ErrorHandler. The error
-// itself is not sent back to the client, but logged instead. The status and
-// optional responseMsg(s) are both used to respond to the client.
-func NewError(err error, status int, responseMsg ...string) error {
-	return &handlerError{
-		err:         err,
-		status:      status,
-		responseMsg: strings.Join(responseMsg, " "),
-	}
 }
